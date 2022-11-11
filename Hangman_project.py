6d43ac33437e4b91721f0baa6f0e8b863b1f5124
gigi <smieja.natalia@gmail.com>
2022-11-11 18:28:34 +0100

    final project

--- Hangman_project.py
+++ Hangman_project.py
@@ -5,10 +5,15 @@
         - cytaty: (pobrano z: https://github.com/JamesFT/Database-Quotes-JSON/blob/master/quotes.json)
         - hasła z podpowiedziami: (pobrano z: https://github.com/le717/PHP-Hangman/tree/master/words)
         - hasła bez podpowiedzi: (pobrano z: https://github.com/hevalhazalkurt/HangmanPy/blob/master/words.txt)
+    Własna modyfikacja projektu:
+        - Dokonano zmian w punkcie "Wybieranie poziomu trudności gry przed jej rozpoczęciem. Będzie to wpływać na ilość prób."
+        - Ze względu na przygotowanie "wizualizacji hangmana" liczba prób odgadnięcia musi być stała. Poziom trudności dotyczy złożoności hasła i ewentualnych podpowiedzi, nie ilości prób.
 '''
 import json
 import random
 import re
+import datetime
+import os
 
 # Basic tools (variables, definitions):
 def random_quote_from_file(file_path: str):
@@ -43,7 +48,7 @@ def random_word_with_hint_from_file(file_path: str):
 
 
 def hidden_guess(quote: str, used_characters: list[str]=[]):
-    '''Changes characters in string into low line ("_").
+    '''Changes characters in string into underscore ("_").
        Special characters like -,.' are not changed.
        Space sign is replaced by double space for better eye-view'''
     final_quote = quote.replace(",","").replace(".","")
@@ -180,34 +185,35 @@ file_path_words_with_hints = r"word-list_from_le717.json"   # from (and with gre
 files = {1:file_path_words_with_hints, 2:file_path_words, 3:file_path_quotes}
 to_guess = ""               # searched word/quote
 max_number_of_attempts = 7  # maximum number of attempts per game
-no_attempts = 0             # number of attempts used by the player
+attempt_no = 0              # number of attempts used by the player
 attempt = ""                # letter/word/quote chosen by the player
 used_letters = []           # letters used by the player
+basic_word = "bulletproof"  # one word entered "permanently" (exam rule no. 1: "jedno hasło wpisane "na stałe")
 
 # ---THE GAME---
 # Introduction
 intro = '''----- INTRODUCTION -----
 The goal of this game is to guess hidden word or quote. 
 The word/quote to guess is represented by a row of dashes representing each letter of the word (only alphabet letters are hidden). 
-You reveal dashes by choosing a letter from alphabet. If the letter occurs in word/quote it is revealed in all its correct positions. 
-If the suggested letter does not occur in the word, you loose one of attempts to guess the word/quote and one element of a hanged stick figure is drawn as a tally mark.
+You reveal dashes by choosing a letter from the alphabet. If the letter occurs in word/quote, it is revealed in all its correct positions. 
+If the suggested letter does not occur in the word, you loose one of the attempts to guess the word/quote and one element of a hanged stick figure is drawn as a tally mark.
 You may, at any time, attempt to guess the whole word/quote. If the word is correct, you win the game. Otherwise, you loose one of attempts to guess the word/quote and another element is added to the hangmen diagram. 
-If all of the of attempts to guess the word/quote are used -  the diagram  of a hangman is complete and the game is lost. 
+If all of the of attempts to guess the word/quote are used, the diagram of a hangman is completed and the game is lost. 
 If you reveal all the letters that appear in the word/quote before the diagram is completed - you win the game.
 ------------------------'''
 
-# Choosing a game level
+# Choosing a game level:
 print(intro+"\n")
 print('''Level of difficulty:
-      1 - low difficulty - word with hint
-      2 - medium difficulty - word without hint
-      3 - high difficulty - quote (Author revealed)
+      0 - basic - one word entered "permanently"
+      1 - low difficulty - random word with a hint
+      2 - medium difficulty - random word without a hint
+      3 - high difficulty - random quote (Author revealed)
       ''')
-
-difficulty_level = input("Choose level of difficulty (type number from 1 to 3): ")
 
 # Selecting a word/quote to guess:
 while True:
+    difficulty_level = input("\nChoose level of difficulty (type number from 0 to 3): ")
     try:
         difficulty_level = int(difficulty_level)
         if difficulty_level == 3:
@@ -221,51 +227,49 @@ while True:
             word = random_word_with_hint_from_file(files[1])
             to_guess = word["word"]
             hint = word["hint"]
+        elif difficulty_level == 0:
+            to_guess = basic_word
         else:
-            assert difficulty_level in [1,2,3], "Chosen number out of range!"
+            assert difficulty_level in [0,1,2,3], "Chosen number out of range!"
     except AssertionError as e:
-        print("--- Error:\n--- Invalid choice - difficulty level must be expressed as a number between 1 and 3.")
+        print("--- Number Error:\n--- Invalid choice - difficulty level must be expressed as a number between 0 and 3.")
         print(f"--- Error code: {e} ---")
-        difficulty_level = input("Choose difficulty level (type number from 1 to 3): ")
     except FileNotFoundError as e:
-        print("--- Error:\n--- External file with quotes is missing. Download file {} from https://github.com/SmiNat/Hangman_project".format(files[int(difficulty_level)]))
+        print("--- Source Error:\n--- External file with quotes is missing. Download file {} from https://github.com/SmiNat/Hangman_project".format(files[int(difficulty_level)]))
         print(f"--- Error code: {e} ---")
-        difficulty_level = input("Choose difficulty level (type number from 1 to 3): ")
     except ValueError as e:
-        print("--- Error:\n--- Invalid choice - the choice must be expressed as a digit.")
-        print(f"--- Error code: {e} Number: {difficulty_level} ---")              # do zastanowienia czemu nie działa wywołanie Number: {}
-        difficulty_level = input("Choose difficulty level (type number from 1 to 3): ")
+        print("--- Type Error:\n--- Invalid choice - the choice must be expressed as a digit.")
+        print(f"--- Error code: {e} Number: {difficulty_level} ---")
     except Exception as e:
         print(f"--- Error:\n--- {e} ---")
-        difficulty_level = input("Choose difficulty level (type number from 1 to 3): ")
     else:
         print("\n"+"-"*30+"\n")
         print(f"Number of guesses: {max_number_of_attempts}")
-        print("Level of difficulty:", end=" ")
-        print("high") if difficulty_level == 3 else print("medium") if difficulty_level == 2 else print("low")
-        print("Category:", end=" ")
-        print("quotes") if difficulty_level == 3 else print("words")
+        print("Level of difficulty: {}".format("high" if difficulty_level == 3 else "medium" if difficulty_level == 2 else "low" if difficulty_level == 1 else "basic"))
+        print("Category: {}".format("quotes" if difficulty_level == 3 else "words"))
         characters = characteristics(to_guess)
         print("\n" + "-" * 30 + "\n")
         print(hangman[0])
         print("To guess: ", end=" ")
         print(hidden_guess(to_guess))
         print("Author: {}".format(author)) if difficulty_level == 3 else print("Hint: {}".format(hint)) if difficulty_level == 1 else print("Hint: none")
+        start = datetime.datetime.now()
         break
 
-print(to_guess)                              # DO UKRYCIA / USUNIĘCIA!
-print(characters)
+# print(to_guess)                              # DO UKRYCIA / USUNIĘCIA! (pola pomocnicze)
+# print(characters)                            # DO UKRYCIA / USUNIĘCIA! (pola pomocnicze)
+
 # Choosing a letter or guessing a word/quote:
-while attempt.replace(" ","") != to_guess.lower().replace(" ","") and no_attempts<max_number_of_attempts:
-    attempt = input("Choose a letter or enter the whole word/quote: ").lower()
+while attempt.replace(" ","") != to_guess.lower().replace(" ","") and attempt_no<max_number_of_attempts:
+    attempt = input("\nChoose a letter or enter the whole word/quote: ").lower()
     if attempt.replace(" ","") == to_guess.lower().replace(" ",""):
         break
-    elif len(attempt)>1:
+    elif len(attempt)>1 and len(attempt) != len(to_guess):
         print("You can only choose one letter at a time or you can try to guess the whole word/quote by typing an answer (the length of the answer must be equal to the length of hidden word/quote).")
     else:
         if attempt.isalpha():
             if attempt in used_letters:
-                print("You have already tried this letter. Choose different letter.")
+                print("You have already tried this letter. Choose a different letter.")
             else:
                 used_letters.append(attempt)
                 if all(item in used_letters for item in characters):
@@ -273,16 +277,36 @@ while attempt.replace(" ","") != to_guess.lower().replace(" ","") and no_attempt
                 elif attempt in to_guess.lower():
                     print("The letter '{}' occurs {} {} in the search word/quote".format(attempt, to_guess.lower().count(attempt), "time" if to_guess.lower().count(attempt)==1 else "times"))
                 else:
-                    no_attempts += 1
-                    print("No letter '{}' found".format(attempt))
-                print(hangman[no_attempts])
-                print(hidden_guess(to_guess, used_letters))
-                print("Attempts used: {}/{}. Letters used: {}".format(no_attempts,max_number_of_attempts, used_letters))
+                    attempt_no += 1
+                    if len(attempt) == len(to_guess) and attempt != to_guess:
+                        print("Nice try but the answer is incorrect.")
+                    else:
+                        print("No letter '{}' found".format(attempt))
+                print("Attempts used: {}/{}. Letters used: {}".format(attempt_no, max_number_of_attempts, used_letters))
+                print(hangman[attempt_no])
+                print("To guess: {}".format(hidden_guess(to_guess, used_letters)))
         else:
             print("Use only alphabet letters.")
-if no_attempts == max_number_of_attempts:
-    print(hangman[no_attempts])
-    print("GAME OVER! You have been hanged!")
+stop = datetime.datetime.now()
+result = ""
+if attempt_no == max_number_of_attempts:
+    print("GAME OVER! You have been hanged!"+"\u2620"*3)
+    result = "LOST"
 else:
-    print("Congratulations! You have found a correct answer!")
+    print("Congratulations! You have found a correct answer!"+"\U0001F44F"*3)
+    result = "WIN"
+print(f"The correct answer was: {to_guess}")
+print("Time of play: {} ({} seconds)".format(stop-start,(stop-start).seconds))
 
+
+# Saving results to a file:
+file_name = r"results.txt"
+current_path = os.getcwd()
+file_path = os.path.join(current_path, file_name)
+
+with open(file_path, "a", newline="\n") as file:
+    log = "Date of play: {}. Result: {}. Number of attempts used: {}/{}. Letters used: {}. Word/quote: {}".format(start.strftime("%Y-%m-%d %H:%M:%S"), result, attempt_no, max_number_of_attempts, used_letters, to_guess)
+    file.write(log)
+print("Data saved in file.")
+
+# Thank you for playing!
